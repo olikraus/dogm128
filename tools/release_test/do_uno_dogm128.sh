22373c0d56f53f142ebdb4df5abed1e3a6f955ba
olikraus@gmail.com <olikraus@gmail.com>
2011-01-02 07:54:43 +0100

    more release test cases

---
+++ tools/release_test/do_uno_dogm128.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# set test configuration
+dut=uno_dogm128
+examples="SpaceTrash GraphicTest Breakorino"
+currdir=`pwd`
+
+# create mount point
+#mkdir release
+
+# note: ensure, that mount can be called via sudo (see visudo)
+#sudo mount -t tmpfs -o size=10M,nr_inodes=5k,mode=0777 tmpfs ./release
+
+for e in  $examples
+do
+
+  cd  $currdir
+
+rm -rf ./release/*	
+
+# copy release
+  cp ../../libraries/dogm128.zip ./release
+
+# unzip release
+  cd release
+  unzip -o dogm128.zip > /dev/null
+
+# copy makefiles
+  cp ../Makefile*${dut}* ./Dogm/examples/$e/.
+
+# execute makefile
+  cd Dogm/examples/$e/
+  for m in Makefile.*
+  do
+    echo START: $e $m
+    pwd
+    make -f $m clean
+    make -f $m upload
+    make -f $m clean
+    echo DONE: $e $m
+    sleep 2
+  done
+
+done
+
+cd  $currdir
+
+#sudo umount ./release
+
